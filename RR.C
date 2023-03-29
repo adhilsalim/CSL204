5019aea0acd967373421da3a887f4eeaf06e1b12
Adhil Salim <bizadhil@gmail.com>
2023-03-29 12:44:50 +0530

    Update RR.C

--- RR.C
+++ RR.C
@@ -22,6 +22,10 @@ void calculateCompletionTime(process[], int);
 void calculateTurnAroundTime(process[], int);
 void calculateWaitingTime(process[], int);
 void calculateAverageWaitingTime(process[], int);
+
+void insertIntoReadyQueue(int);
+int deleteFromReadyQueue();
+
 bool isTableBreak(int);
 bool isTableDataField(int);
 bool isArrivalTime(process[], int, int);
@@ -38,7 +42,46 @@ int readyQueueRear = -1;
 
 // System Clock
 int systemClock = 0;
+int currentProccess = -1;
 
+// Ready Queue Functions
+void insertIntoReadyQueue(int processId)
+{
+    if (readyQueueRear == -1 && readyQueueFront == -1)
+    {
+        readyQueueRear++;
+        readyQueueFront++;
+        readyQueue[readyQueueRear] = processId;
+    }
+    else if (readyQueueRear != 100)
+    {
+        readyQueueRear++;
+        readyQueue[readyQueueRear] = processId;
+    }
+}
+
+int deleteFromReadyQueue()
+{
+    if (readyQueueFront == -1 && readyQueueRear == -1)
+    {
+        return -1;
+    }
+    else if (readyQueueFront == readyQueueRear)
+    {
+        int temp = readyQueue[readyQueueFront];
+        readyQueueFront = -1;
+        readyQueueRear = -1;
+        return temp;
+    }
+    else
+    {
+        int temp = readyQueue[readyQueueFront];
+        readyQueueFront++;
+        return temp;
+    }
+}
+
+// Getting process details
 void getProcessDetails(process p[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -62,4 +105,20 @@ void main()
     process p[n];
 
     getProcessDetails(p, n);
+
+    for (systemClock = 0; systemClock <= 14; systemClock++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (p[i].arrivalTime == systemClock)
+            {
+                insertIntoReadyQueue(p[i].processId);
+            }
+        }
+
+        if (currentProccess == -1)
+        {
+            currentProccess = readyQueue[++readyQueueFront];
+        }
+    }
 }
