8a8a1e85d5d629f16783fe2ff336ff90b4476f44
Adhil Salim <bizadhil@gmail.com>
2023-08-12 14:46:49 +0530

    calc ct tat wt

--- Process Scheduling/FCFS.c
+++ Process Scheduling/FCFS.c
@@ -63,4 +63,45 @@ void main()
             }
         }
     }
+
+    // calculating the CT, TAT, WT for each process
+    p[0].CT = p[0].AT + p[0].BT;
+    p[0].TAT = p[0].CT - p[0].AT;
+    p[0].WT = p[0].TAT - p[0].BT;
+
+    int current_time = p[0].CT;
+
+    for (int i = 1; i < total_processes; i++)
+    {
+        if (p[i].AT > current_time)
+        {
+            current_time = p[i].AT;
+        }
+
+        p[i].CT = current_time + p[i].BT;
+        p[i].TAT = p[i].CT - p[i].AT;
+        p[i].WT = p[i].TAT - p[i].BT;
+
+        current_time = p[i].CT;
+    }
+
+    // printing the TAT and WT for each process
+    // also calculating the average TAT and WT
+    float avg_TAT = 0;
+    float avg_WT = 0;
+
+    for (int i = 0; i < total_processes; i++)
+    {
+        printf("Process %d:\n", p[i].PID);
+        printf("Turn around time: %d\n", p[i].TAT);
+        printf("Waiting time: %d\n", p[i].WT);
+        printf("\n");
+
+        avg_TAT += p[i].TAT;
+        avg_WT += p[i].WT;
+    }
+
+    // printing the average TAT and WT
+    printf("Average turn around time: %2.f\n", avg_TAT / total_processes);
+    printf("Average waiting time: %2.f\n", avg_WT / total_processes);
 }
