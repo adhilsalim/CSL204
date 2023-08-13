6c4f80e396734cd8bb5f75d076384425e5eaf6a0
Adhil Salim <bizadhil@gmail.com>
2023-08-13 12:06:33 +0530

    Update FCFS.c

--- Disk Scheduling/FCFS.c
+++ Disk Scheduling/FCFS.c
@@ -4,20 +4,23 @@
 
 int getSeek(int, int);
 
-int getSeek(int postion_1, int position_2){
-    int seek = postion_1-position_2;
+int getSeek(int postion_1, int position_2)
+{
+    int seek = postion_1 - position_2;
 
     // that is negative number
-    if(seek<0)
+    if (seek < 0)
         return (seek * -1);
 
     return seek;
 }
 
-void main(){
+void main()
+{
 
     int head_pos, disk_range, number_of_tracks;
 
+    // get head position, disk range, number of tracks
     printf("Enter the current of head position: ");
     scanf("%d", &head_pos);
 
@@ -27,32 +30,39 @@ void main(){
     printf("Enter the number of tracks to access: ");
     scanf("%d", &number_of_tracks);
 
-    int TRACKS[number_of_tracks+1]; // including head position
-    TRACKS[0] = head_pos; // adding head pos to array
+    int TRACKS[number_of_tracks + 1]; // why +1? because we have to include the head position
+    TRACKS[0] = head_pos;             // adding head pos to array
 
-    for(int i=1; i<=number_of_tracks; i++){
+    // get tracks
+    for (int i = 1; i <= number_of_tracks; i++)
+    {
         printf("Enter the track %d: ", i);
-        scanf("%d",&TRACKS[i]);
+        scanf("%d", &TRACKS[i]);
 
-        if(TRACKS[i]>disk_range || TRACKS[i]<0){
+        // check if track is out of range
+        if (TRACKS[i] > disk_range || TRACKS[i] < 0)
+        {
             printf("\nTRACK EXCEEDED DISK RANGE. EXITING PROGRAM.\n");
             exit(0);
         }
     }
 
+    // print tracks
     printf("\nTRACKS ARRAY: \n");
-    for(int i=0; i<=number_of_tracks; i++){
+    for (int i = 0; i <= number_of_tracks; i++)
+    {
         printf("%d ", TRACKS[i]);
     }
     printf("\n\n");
-    
-    int seek_sum=0;
 
-    for(int i=0; i<=number_of_tracks-1; i++){ // -1 cuz we don't have the last elm after array bound
-        int seek = getSeek(TRACKS[i], TRACKS[i+1]);
-        seek_sum+=seek;
-        printf("Head moves from %d to %d with seek %d\n", TRACKS[i], TRACKS[i+1], seek);
+    int seek_sum = 0;
+
+    for (int i = 0; i <= number_of_tracks - 1; i++)
+    { // -1 cuz we don't have the last element after array bound
+        int seek = getSeek(TRACKS[i], TRACKS[i + 1]);
+        seek_sum += seek;
+        printf("Head moves from %d to %d with seek %d\n", TRACKS[i], TRACKS[i + 1], seek);
     }
 
-    printf("\nTOTAL SEEKS: %d\nAVG SEEK: %d\n", seek_sum, (seek_sum/number_of_tracks)); // num_tracks will be total seeks
+    printf("\nTOTAL SEEKS: %d\nAVG SEEK: %d\n", seek_sum, (seek_sum / number_of_tracks)); // num_tracks will be total seeks
 }
