068ebc85114ba1898bdfe0fc0334a74126e63655
Adhil Salim <bizadhil@gmail.com>
2023-08-13 11:06:42 +0530

    create WF

---
+++ Memory Allocation/WF.c
@@ -0,0 +1,23 @@
+/*
+    Worst Fit Algorithm
+        SAMPLE INPUT:
+
+        Enter the number of processes: 4
+        Enter the total memory available: 5
+        Enter the memory required for each process:
+        Process 1: 212
+        Process 2: 417
+        Process 3: 112
+        Process 4: 426
+        Enter the memory available for each block:
+        Block 1: 600
+        Block 2: 500
+        Block 3: 300
+        Block 4: 200
+        Block 5: 100
+
+        Here i have used the same code as First Fit Algorithm, but the only difference is that i have sorted the memory array in descending order.
+        That is the only difference between First Fit and Worst Fit Algorithm is that in Worst Fit Algorithm we sort the memory array in descending order.
+
+        try the above input in FF.c
+*/
