dd6141a2e5c466422c5ae9e3675611a2e317168b
Adhil Salim <bizadhil@gmail.com>
2023-11-28 09:57:29 +0530

    add WFA

--- Memory Allocation/WF.c
+++ Memory Allocation/WF.c
@@ -1,23 +1,65 @@
-/*
-    Worst Fit Algorithm
-        SAMPLE INPUT:
+#include <stdio.h>
 
-        Enter the number of processes: 4
-        Enter the total memory available: 5
-        Enter the memory required for each process:
-        Process 1: 212
-        Process 2: 417
-        Process 3: 112
-        Process 4: 426
-        Enter the memory available for each block:
-        Block 1: 600
-        Block 2: 500
-        Block 3: 300
-        Block 4: 200
-        Block 5: 100
+void main()
+{
+    int total_processes, total_memory;
 
-        Here i have used the same code as First Fit Algorithm, but the only difference is that i have sorted the memory array in descending order.
-        That is the only difference between First Fit and Worst Fit Algorithm is that in Worst Fit Algorithm we sort the memory array in descending order.
+    printf("Enter the number of processes: ");
+    scanf("%d", &total_processes);
 
-        try the above input in FF.c
-*/
+    printf("Enter the total memory available: ");
+    scanf("%d", &total_memory);
+
+    int process_size[2][total_processes];
+    int memory[2][total_memory];
+
+    printf("Enter the memory required for each process: \n");
+    for (int i = 0; i < total_processes; i++)
+    {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &process_size[0][i]);
+        process_size[1][i] = 0; // flag
+    }
+
+    printf("Enter the memory available for each block: \n");
+    for (int i = 0; i < total_memory; i++)
+    {
+        printf("Block %d: ", i + 1);
+        scanf("%d", &memory[0][i]);
+        memory[1][i] = 0; // flag
+    }
+
+    printf("\nAllocated processes: \n");
+
+    for (int i = 0; i < total_processes; i++)
+    {
+        int worst_fit_index = -1;
+
+        for (int j = 0; j < total_memory; j++)
+        {
+            if (memory[1][j] == 0 && memory[0][j] >= process_size[0][i])
+            {
+                if (worst_fit_index == -1 || memory[0][j] > memory[0][worst_fit_index])
+                {
+                    worst_fit_index = j;
+                }
+            }
+        }
+
+        if (worst_fit_index != -1)
+        {
+            memory[1][worst_fit_index] = 1;
+            process_size[1][i] = 1;
+            printf("Process %d is allocated to Block %d\n", i + 1, worst_fit_index + 1);
+        }
+    }
+
+    printf("\nUnallocated processes: ");
+    for (int i = 0; i < total_processes; i++)
+    {
+        if (process_size[1][i] == 0)
+        {
+            printf("%d ", i + 1);
+        }
+    }
+}
