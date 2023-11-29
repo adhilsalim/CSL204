06d5170e3f5d144861e39a9c5013724dd3d6cb79
Adhil Salim <bizadhil@gmail.com>
2023-11-29 10:02:17 +0530

    add BFA

--- Memory Allocation/BF.c
+++ Memory Allocation/BF.c
@@ -1,23 +1,78 @@
-/*
-    Best Fit Algorithm
-        SAMPLE INPUT:
+#include <stdio.h>
 
-        Enter the number of processes: 4
-        Enter the total memory available: 5
-        Enter the memory required for each process:
-        Process 1: 212
-        Process 2: 417
-        Process 3: 112
-        Process 4: 426
-        Enter the memory available for each block:
-        Block 1: 100
-        Block 2: 200
-        Block 3: 300
-        Block 4: 500
-        Block 5: 600
+int main()
+{
+    int total_processes, total_memory;
 
-        Here i have used the same code as First Fit Algorithm, but the only difference is that i have sorted the memory array in ascending order.
-        That is the only difference between First Fit and Best Fit Algorithm is that in Best Fit Algorithm we sort the memory array in ascending order.
+    // Get the number of processes and available memory
+    printf("Enter the number of processes: ");
+    scanf("%d", &total_processes);
 
-        try the above input in FF.c
-*/
+    printf("Enter the total memory available: ");
+    scanf("%d", &total_memory);
+
+    // Initialize arrays for process size, memory size, and allocation flags
+    int process_size[2][total_processes];
+    int memory[2][total_memory];
+
+    // Get the memory required for each process
+    printf("Enter the memory required for each process:\n");
+    for (int i = 0; i < total_processes; i++)
+    {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &process_size[0][i]);
+        process_size[1][i] = 0; // flag
+    }
+
+    // Get the memory available for each block
+    printf("Enter the memory available for each block:\n");
+    for (int i = 0; i < total_memory; i++)
+    {
+        printf("Block %d: ", i + 1);
+        scanf("%d", &memory[0][i]);
+        memory[1][i] = 0; // flag
+    }
+
+    // Allocate memory using best-fit algorithm
+    printf("\nAllocated processes:\n");
+    for (int i = 0; i < total_processes; i++)
+    {
+        int best_fit_index = -1;
+        int best_fit_remaining_memory = total_memory;
+
+        // Find the best fit block among available ones
+        for (int j = 0; j < total_memory; j++)
+        {
+            if (memory[1][j] == 0 && memory[0][j] >= process_size[0][i])
+            {
+                int remaining_memory = memory[0][j] - process_size[0][i];
+                if (best_fit_index == -1 || remaining_memory < best_fit_remaining_memory)
+                {
+                    best_fit_index = j;
+                    best_fit_remaining_memory = remaining_memory;
+                }
+            }
+        }
+
+        // Allocate the best fit block if found
+        if (best_fit_index != -1)
+        {
+            memory[1][best_fit_index] = 1;
+            process_size[1][i] = 1;
+            printf("Process %d is allocated to Block %d\n", i + 1, best_fit_index + 1);
+        }
+    }
+
+    // Print unallocated processes
+    printf("\nUnallocated processes: ");
+    for (int i = 0; i < total_processes; i++)
+    {
+        if (process_size[1][i] == 0)
+        {
+            printf("%d ", i + 1);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
